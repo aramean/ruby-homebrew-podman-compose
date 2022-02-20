abf9320779c2e2a1feb637ce2b5e37183fd94dd1
Josef Gabrielsson <gabrielsson@josef.st>
2022-02-20 01:00:52 +0100

    Update podman-compose.rb

--- podman-compose.rb
+++ podman-compose.rb
@@ -1,26 +1,27 @@
-require "language/go"
-
 class PodmanCompose < Formula
   desc "Podman Compose lets you define and run multi-container Podman applications."
   homepage "https://aramean.github.io/go-podman-compose"
-  url "https://github.com/aramean/go-podman-compose/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/aramean/go-podman-compose/archive/v1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "b704dbb36d167c2cbd04b050dbcf51641973db4e5cf714c38c60c2d090aaf353"
+  sha256 "6f2bd9a6d638f1524db63dfecbe3cb7cee611b801033dcc301151acbe3e74d89"
   license "MIT"
-  
-  depends_on "go" => :build
+
+  depends_on "go"
 
   def install
     ENV["GOPATH"] = buildpath
-    path = buildpath/"src/github.com/aramean/go-podman-compose"
-    path.install Dir["*"]
-    cd path do
-      system "go", "build", "-o", "#{bin}/podman-compose"
+    ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
+    file = "podman-compose"
+    source = "src/github.com/aramean/go-podman-compose"
+    (buildpath/source).install buildpath.children
+    cd source do
+      system "go", "get", "./..."
+      system "go", "build", "-o", bin/file, "."
     end
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/podman-compose", "-v")
-    system "false"
+    file = "podman-compose"
+    assert_match "#{file} version #{version}", shell_output("#{bin}/#{file} version")
   end
 end
