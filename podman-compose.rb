7266615af4f6cb9c4ea6956bb33ae3e0b6eb1e6e
Josef Gabrielsson <gabrielsson@josef.st>
2022-02-22 22:40:53 +0100

    Update podman-compose.rb

--- podman-compose.rb
+++ podman-compose.rb
@@ -2,7 +2,6 @@ class PodmanCompose < Formula
   desc "Tool for managing multicontainer Podman applications via YAML files"
   homepage "https://aramean.github.io/go-podman-compose"
   url "https://github.com/aramean/go-podman-compose/archive/v1.0.0.tar.gz"
-  version "1.0.0"
   sha256 "6f2bd9a6d638f1524db63dfecbe3cb7cee611b801033dcc301151acbe3e74d89"
   license "MIT"
 
